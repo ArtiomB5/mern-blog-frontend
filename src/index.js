a0bbc32827f6d3468f54a7e732e13bf2fb84f6a7
Artiom <artiomb5@yandex.by>
2023-03-21 22:03:34 +0600

    posts rendering

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
-
-import { BrowserRouter } from "react-router-dom"
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
